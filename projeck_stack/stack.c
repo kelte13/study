6697c59de6b5c56c77bdf5e130ea8753cab61d56
kelte13 <dim.tcka4ew@gmail.com>
2023-10-24 23:09:52 +0300

    Create stack.c

---
+++ projeck_stack/stack.c
@@ -0,0 +1,52 @@
+#include "stack.h"
+int main() {
+    for (int i = 0; i < 7; i++) {
+        push(i);
+    }
+    show();
+    printf("\n");
+    for (int i = 0; i < 5; i++) {
+        pop();
+    }
+    printf("\n");
+    show();
+    return 0;
+}
+
+void show(void) {
+    for (int i = 0; i < first; i++)
+        printf("%d", arr[i]);
+}
+
+int push(int n) {
+    if (is_full() == 0) {
+        arr[first++] = n;
+        return 0;
+    }
+    else
+        return -1;
+}
+
+int pop(void) {
+    if (is_empty() == 1)
+        return -1;
+    else {
+        printf("%d", arr[--first]);
+        arr[first] = 0;
+    }
+}
+
+_Bool is_full(void) {
+    if (first == MAXOP) {
+        return 1;
+    }
+    return 0;
+}
+
+_Bool is_empty(void) {
+    if (first == 0)
+        return 1;
+    else
+        return 0;
+
+}
