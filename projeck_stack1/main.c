4b666af87ca4c61162a9270a5a41ff09ccf9924d
kelte13 <dim.tcka4ew@gmail.com>
2023-10-25 16:11:43 +0300

    Create main.c

---
+++ projeck_stack1/main.c
@@ -0,0 +1,16 @@
+#include "stack.h"
+#include "stack.c"
+int main() {
+    for (int i = 0; i < 7; i++) {
+        push(i);
+    }
+    show();
+    printf("\n");
+    for (int i = 0; i < 5; i++) {
+        pop();
+    }
+    printf("\n");
+    show();
+    return 0;
+}
+
